3a1c36bc78fa6fdcd7407dadbf15178d332e926d
Ha-limLee <leehalim09@gmail.com>
2024-07-30 23:28:22 +0900

    ♻️ remove non null assertion

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const root = document.getElementById("root");
+if (!root) throw new Error("cannot find root element");
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
