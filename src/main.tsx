861465222ccd5ce58448932972fc03a4450c9fc7
Ha-limLee <leehalim09@gmail.com>
2024-08-03 23:25:44 +0900

    💄 set root layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app.tsx";
+import "./main.module.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("cannot find root element");
